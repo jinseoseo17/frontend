e92a51dfb0a3074245863b85d0506f6d20d5dcad
jinseoseo17 <cyc09167@naver.com>
2024-04-30 17:35:14 +0900

    router prac

---
+++ 0430/router_prac/src/views/NumberView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      {{ num }}
+    </h1>
+  </div>
+</template>
+
+<script setup>
+import {useRoute} from 'vue-router'
+const route = useRoute()
+
+const num = route.params.num
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
